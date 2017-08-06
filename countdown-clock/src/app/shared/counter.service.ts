fd33da354a448cd95153ca26ef5419c7ec17e818
josel82 <jose.padilla.dev@gmail.com>
2017-08-06 11:55:16 +0100

    updated counter.service.ts, adding a new feature where the app throws a warning message if the user inserts a invalid time. That is, if the time inserted is before the current time

--- countdown-clock/src/app/shared/counter.service.ts
+++ countdown-clock/src/app/shared/counter.service.ts
@@ -21,7 +21,7 @@ export class CounterService{
     let s = date.getSeconds();
     return [h,m,s];
   }
-
+  //current time generator
   timeEngine = setInterval(()=>{
        let h = this.getTime()[0];
        this.time.hours = h < 10 ? '0'+h.toString(): h.toString();
@@ -31,37 +31,47 @@ export class CounterService{
 
        let s = this.getTime()[2];
        this.time.seconds = s < 10 ? '0'+s.toString(): s.toString();
+
+       //Activates the Countdown clock
        if(this.countingDown){
          this.diff(this.input,this.getTime());
        }
    }, 1000);
 
+ //Countdown clock
   diff(t2,t1){
 
     let time2 = (t2[0]*3600000)+(t2[1]*60000)+(t2[2]*1000);
     let time1 = (t1[0]*3600000)+(t1[1]*60000)+(t1[2]*1000);
     let res = time2 - time1;
-    if(res <= 0){
-       this.flag.next(true);
-       this.countingDown = false;
+
+    if(res < 0 && this.countingDown){ //checks for invalid time. that is, case time inserted is less than current time
+      alert("Please insert a valid time!");
+      this.countingDown = false;
+      return;
+    }else if(res <= 0){ //checks if the time is out
+       this.flag.next(true); //turns the "On Air" sign on
+       this.countingDown = false; // stops the countdown clock.
+
     }
+      //time difference calculation
+      let h = Math.floor(res/3600000);
+      let hour = h < 10 ? '0'+h.toString() : h.toString();
+      let m = Math.floor((res%3600000)/60000);
+      let min = m < 10 ? '0'+m.toString() : m.toString();
+      let s = Math.floor(((res%3600000)%60000)/1000);
+      let sec = s < 10 ? '0'+s.toString() : s.toString();
 
-    let h = Math.floor(res/3600000);
-    let hour = h < 10 ? '0'+h.toString() : h.toString();
-    let m = Math.floor((res%3600000)/60000);
-    let min = m < 10 ? '0'+m.toString() : m.toString();
-    let s = Math.floor(((res%3600000)%60000)/1000);
-    let sec = s < 10 ? '0'+s.toString() : s.toString();
-
-    this.timeUpdated.next([hour,min,sec]);
-
+      this.timeUpdated.next([hour,min,sec]);
   }
 
+//Called when the start button is clicked
   setCounter(input){
     this.input = input;
     this.countingDown = true;
   }
 
+//Called when the "clear" button is clicked
   onClear(){
     this.countingDown = false;
     this.flag.next(false);
